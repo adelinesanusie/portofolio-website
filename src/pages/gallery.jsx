fac30b2722b68f96d7fc3adcd4631944dbf92a01
Adeline Sonia Sanusie <adeline.sonia@ui.ac.id>
2023-06-21 01:34:33 +0700

    Add swiper react components to gallery

--- src/pages/gallery.jsx
+++ src/pages/gallery.jsx
@@ -1,25 +1,59 @@
-import Spiderman from "../assets/Spiderman.jpg"
-import Thor from "../assets/Thor.jpg"
-import Marvel from "../assets/Marvel.jpg"
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Navigation, Autoplay } from "swiper";
+import "swiper/swiper.min.css";
+
+import Spiderman from "../assets/Spiderman.jpg";
+import Thor from "../assets/Thor.jpg";
+import Marvel from "../assets/Marvel.jpg";
+import TheIncredibles from "../assets/TheIncredibles.jpg";
+import Yoda from "../assets/Yoda.jpg";
+import KobeGianna from "../assets/KobeGianna.jpg";
+import HarryPotter from "../assets/HarryPotter.jpg";
+import TheBeatles from "../assets/TheBeatles.jpg";
+import NelsonMandela from "../assets/NelsonMandela.jpg";
+
+// Install Swiper modules
+SwiperCore.use([Navigation, Autoplay]);
 
 function Gallery() {
-    
-    return (
-        <div class="gallery" id="gallery">
-        <div class="container-gallery">
-            <div class="title-gallery">
-                <h2>DRAWING COLLECTIONS</h2>
-                <p>
-                </p>
-            </div>
-            <div class="draw">
-                <img src={Spiderman}/>
-                <img src={Thor}/>
-                <img src={Marvel}/>
-            </div>
+  const images = [
+    { id: 1, src: Spiderman, alt: "Spiderman" },
+    { id: 2, src: Thor, alt: "Thor" },
+    { id: 3, src: Marvel, alt: "Marvel" },
+    { id: 4, src: TheIncredibles, alt: "TheIncredibles" },
+    { id: 5, src: Yoda, alt: "Yoda" },
+    { id: 6, src: KobeGianna, alt: "KobeGianna" },
+    { id: 7, src: HarryPotter, alt: "HarryPotter" },
+    { id: 8, src: TheBeatles, alt: "TheBeatles" },
+    { id: 9, src: NelsonMandela, alt: "NelsonMandela" },
+  ];
+
+  return (
+    <div className="gallery" id="gallery">
+      <div className="container-gallery swiper-container"> {/* Added "swiper-container" class */}
+        <div className="title-gallery">
+          <h2>DRAWING COLLECTIONS</h2>
+          <p></p>
         </div>
-        </div>
-    );
+        <Swiper
+          spaceBetween={50}
+          slidesPerView={3}
+          navigation
+          loop
+          autoplay={{ delay: 2500 }}
+          onSlideChange={() => console.log("slide change")}
+          onSwiper={(swiper) => console.log(swiper)}
+        >
+          {images.map((image) => (
+            <SwiperSlide key={image.id}>
+              <img src={image.src} alt={image.alt} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </div>
+  );
 }
-  
+
 export default Gallery;
