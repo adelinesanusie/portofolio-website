865807de28b58f7f7619c40568311bc1005dde10
Adeline Sonia Sanusie <adeline.sonia@ui.ac.id>
2023-07-04 05:15:44 +0700

    Add responsive for mobile size L

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import './styles/index-light.css'
 import './styles/index-dark.css'
 import './styles/responsive-laptop1024.css'
 import './styles/responsive-tablet.css'
+import './styles/responsive-mobileL425.css'
+import './styles/responsive-mobileM375.css'
 
 import Main from './pages/main';
 function App() {
